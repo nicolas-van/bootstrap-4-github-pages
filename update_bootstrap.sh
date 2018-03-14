a39da7c7834ade80c635b31592abfc5437bacd7f
Nicolas Vanhoren <n.vanhoren@evs.com>
2018-03-14 13:15:17 +0100

    wip

--- update_bootstrap.sh
+++ update_bootstrap.sh
@@ -6,8 +6,10 @@ npm install bootstrap@4 jquery@3
 rm -rf _sass/bootstrap
 mkdir -p _sass/bootstrap
 cp -r node_modules/bootstrap/scss/* _sass/bootstrap
+touch _sass/bootstrap/__DO_NOT_MODIFY
 
 rm -rf js/bootstrap
 mkdir -p js/bootstrap
 cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.* js/bootstrap/
 cp node_modules/jquery/dist/jquery.min.* js/bootstrap/
+touch js/bootstrap/__DO_NOT_MODIFY
