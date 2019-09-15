6c2cedbcaff096c5d1473184a7349d81e4ef14e0
Jerry Smith <jsmith@jerry-smith.net>
2019-09-14 21:31:51 -0500

    Updated _config.yml

--- start-test-server.sh
+++ start-test-server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-podman run --security-opt label=disable -it --rm -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages;
+# podman doesn't support port bindings on rootless containers yet
+sudo podman run --security-opt label=disable -it --rm -v "$PWD":/usr/src/app -p "4000:4000" starefossen/github-pages;
