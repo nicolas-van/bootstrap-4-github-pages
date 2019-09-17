0e0ac24651cbd0440463adb638bc797908336e5d
Jerry Smith <jsmith@jerry-smith.net>
2019-09-17 14:40:42 -0500

    Ready for first release

--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -2,12 +2,12 @@ $(document).ready(() => {
   let description_field = $('textarea[name="description"]');
   let max_characters = description_field.attr('maxlength');
 
-  $('#count_message').html(max_characters + ' remaining');
+  $('#count_message').html(max_characters + ' characters remaining');
 
   description_field.keyup(() => {
     let length = description_field.val().length;
     let remaining = max_characters - length;
     
-    $('#count_message').html(remaining + ' remaining');
+    $('#count_message').html(remaining + ' characters remaining');
   });
 });
--- index.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-title: About Me
-icon: <i class="fas fa-id-badge"></i>
----
-
-Page content
