8b7affe86586f154d1607f480eacf736fd4e7a6c
eugrro <eugene.rozental@gmail.com>
2019-11-02 22:48:50 -0400

    Update data.php

--- data.php
+++ data.php
@@ -1,1 +1,9 @@
-1
+<?php
+
+ $filename="datatest.html";
+ file_put_contents($filename,$_POST["fname"]."<br />",FILE_APPEND);
+ file_put_contents($filename,$_POST["fphone"]."<br />",FILE_APPEND);
+ file_put_contents($filename,$_POST["femail"]."<br />",FILE_APPEND);
+ file_put_contents($filename,$_POST["fcomment"]."<br />",FILE_APPEND);
+ $msg=file_get_contents($filename);
+ echo $msg; ?>
