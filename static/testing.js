2f4a05e67f5872a151116099df3ac98ed5c28fc2
eugrro <eugene.rozental@gmail.com>
2019-11-03 02:51:36 -0500

    Create testing.js

---
+++ static/testing.js
@@ -0,0 +1,19 @@
+var ref = firebase.database().ref();
+var name = 'John';
+var age = '21';
+var lat = '75.6';
+var long = '80.9';
+
+function info(name, lat, long, age){
+   console.log(name);
+   console.log(age);
+   console.log(lat);
+   console.log(long);
+}
+
+ref.on("value", function(snapshot) {
+   console.log(snapshot.val());
+}, function (error) {
+   console.log("Error: " + error.code);
+});
+info(name, age, lat, long);
