ac4191fcb23d0253d37e5d0999b68c89c4748eca
eugrro <eugene.rozental@gmail.com>
2019-11-03 00:17:58 -0400

    Update test.js

--- static/test.js
+++ static/test.js
@@ -1,1 +1,15 @@
-q
+function saveToFirebase(email) {
+    var emailObject = {
+        email: email
+    };
+
+    firebase.database().ref('subscription-entries').push().set(emailObject)
+        .then(function(snapshot) {
+            success(); // some success method
+        }, function(error) {
+            console.log('error' + error);
+            error(); // some error method
+        });
+}
+
+saveToFirebase("email@gmail.com");
