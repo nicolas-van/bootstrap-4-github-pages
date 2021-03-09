e3caee8a35ab89c85f1f78ee4e1e4e56ccec7e5c
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2021-03-09 15:29:20 +0100

    Updated dependencies

--- update_bootstrap.sh
+++ update_bootstrap.sh
@@ -2,7 +2,7 @@
 
 # A useful script to download the latest version of bootstrap and jquery
 
-rm -rf node_modules package-lock.json
+rm -rf node_modules package.json package-lock.json
 npm install bootstrap@4 jquery@3
 
 rm -rf _sass/bootstrap
@@ -15,3 +15,5 @@ mkdir -p assets/javascript/bootstrap
 cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.* assets/javascript/bootstrap/
 cp node_modules/jquery/dist/jquery.min.* assets/javascript/bootstrap/
 touch assets/javascript/bootstrap/__DO_NOT_MODIFY
+
+rm -rf node_modules
